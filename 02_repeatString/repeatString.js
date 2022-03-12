9871ca203bbe6ecb633eb0063ba6b0f77f322883
R. S. Blackrose <rsblackrose@wi.rr.com>
2022-03-12 16:18:01 -0600

    Exercise #2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) return "ERROR";
 
+  let retString = "";
+  for (let i = 1; i <= num; ++i) {
+    retString += string;
+  }
+  return retString;
 };
 
 // Do not edit below this line
