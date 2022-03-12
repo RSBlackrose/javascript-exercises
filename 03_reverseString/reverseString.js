452720f19991a22b65ccaa20e767476cb5c9fec4
R. S. Blackrose <rsblackrose@wi.rr.com>
2022-03-12 16:26:21 -0600

    Exercise #3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  if (string === "") return string;
 
+  const strArray = string.split("");
+  const revArray = strArray.reverse(string);
+  const joinArray = revArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
