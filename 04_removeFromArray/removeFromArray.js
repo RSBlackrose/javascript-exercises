cb1cc7ece3e28f80892f3cbca6ec1fd0a39a8482
R. S. Blackrose <rsblackrose@wi.rr.com>
2022-03-12 16:41:52 -0600

    Exercise #4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  args.forEach(arg => {
+    const indexPos = array.findIndex((element) => element === arg);
+    if (indexPos >= 0) array.splice(indexPos,1);
+  });
+  return array;
 };
 
 // Do not edit below this line
