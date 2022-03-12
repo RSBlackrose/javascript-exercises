7482f6310ca2a013df02d9adfc088cdd548ae5da
R. S. Blackrose <rsblackrose@wi.rr.com>
2022-03-12 16:54:59 -0600

    Exercise #5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let upperBounds, lowerBounds, sum;
+  
+  if (num2 < num1) {
+    lowerBounds = num2;
+    upperBounds = num1;
+  } 
+  else {
+    lowerBounds = num1;
+    upperBounds = num2;
+  }
 
+  if (num2 < 0 || num1 < 0) return "ERROR";
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+
+  sum = 0;
+  for (let i = lowerBounds; i <= upperBounds; ++i) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
